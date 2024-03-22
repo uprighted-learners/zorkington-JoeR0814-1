7b6f4c8ef66ad8dadb310c0821d5ee1d81b5a5d8
JoeR0814 <joereis0814@gmail.com>
2024-03-21 20:07:05 -0400

    this is what i have so far, i have some story line started

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+const rl = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
-    readlineInterface.question(questionText, resolve);
+    rl.question(questionText, resolve);
   });
 }
 
@@ -18,3 +18,70 @@ On the door is a handwritten sign.`;
   console.log('Now write your code to make this work!');
   process.exit();
 }
+
+class room {
+  constructor(description, items, exit) {
+    (this.description = description), (this.items = items), (this.exit = exit);
+  }
+}
+
+let room1 = new room({
+  name: 'room1'
+  description: '',
+  items: '',
+  exit: '',
+});
+
+let room2 = new room {
+  name: 'room2'
+  description: '',
+  items: '',
+  exit: '',
+};
+
+let room3 = new room ({
+  name: 'room3',
+  description: '',
+  items: '',
+  exit: ''
+});
+
+let room4 = new room ({
+  name: 'room4',
+  description: '',
+  items: '',
+  exit: '',
+});
+
+let room5 = new room ({
+  name: 'room5',
+  description: '',
+  items: ''
+  exit: '',
+});
+
+let room6 = new room ({
+  name: 'room6',
+  description: '',
+  items: '',
+  exit: '',
+});
+console.log(room);
+locationLookUp = {
+  room1: 'room1',
+  room2: 'room2',
+  room3: 'room3',
+  room4: 'room4',
+  room5: 'room5',
+  room6: 'room6',
+};
+
+locationState = {
+  room1: ['room2', 'room3'],
+  room2: ['room1', 'room3'],
+  room3: ['room2', 'room4'],
+  room4: ['room3', 'room5'],
+  room5: ['room4', 'room6'],
+  room6: ['room5', 'room1'],
+};
+
