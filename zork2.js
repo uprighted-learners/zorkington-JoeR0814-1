478aae0a5403c7210458393d1dfdd44cc14416b9
JoeR0814 <joereis0814@gmail.com>
2024-05-05 11:30:45 -0400

    hey i fished i think it is working to the way the instructions say

--- zork2.js
+++ zork2.js
@@ -10,7 +10,7 @@ function ask(questionText) {
 // an async function to start the game
 async function startGame() {
   const welcomeMessage =
-    'Welcome home, you just got off the school bus and your parents are not home. You have to get in the house by yourself through the garage, you mom left the garage unlocked for you to get in, all you have to do is wash up, eat a snack and go relax in your room.';
+    'Welcome home, you just got off the school bus and your parents are not home. from the street You have to head north into the drive and walk to the garage, to get in the house. Your mom left the garage unlocked for you to get in, you have to just lift the garage, get in through the basemen, wash up in the bathroom, eat a snack and go relax in your room.';
   console.log(welcomeMessage);
   // let answer = await ask('>_');
   // playerCommands(answer);
@@ -33,46 +33,75 @@ class Room {
   }
 }
 
-console.log(Room);
-
 // this is the roomState object
 const rooms = {
   driveway: {
     description:
-      'You are standing in a driveway the street is south the garage is north. ',
+      'Your standing in the street head {north} into the drive to get in the garage ',
     connection: ['garage', 'street'],
     inventory: [],
   },
   garage: {
-    description: 'You have to {open} garage to get to the basement.',
+    description: 'You have to {lift} the garage to get to the basement.',
     connection: ['driveway', 'basement'],
     inventory: [],
   },
   basement: {
-    description: 'You are in the basement. There is a door to the north.',
+    description:
+      'You are in the garage, you have to {open} the door to get into the basemen.',
     connection: ['garage', 'bathroom'],
     inventory: [],
   },
   bathroom: {
     description:
-      'You are in the bathroom. There is a door to the north and south.',
+      'You are in the basement, There is a door to get into the bathroom to wash up, the sink is there for water, soap and the towel is there for you ot {wash up}.',
     connection: ['basement', 'kitchen', 'bedroom'],
     inventory: ['soap', 'water', 'towel'],
   },
   kitchen: {
     description:
-      'You are in the kitchen. There is a door to the north and south.',
-    connection: ['bathroom', 'kitchen', 'bedroom'],
+      'after the bathroom, head into the kitchen to make yourself a {snack}.',
+    connection: ['bathroom', 'bedroom'],
     inventory: ['ham', 'cheese', 'bread', 'plate'],
   },
   bedroom: {
-    description: 'You are in the bedroom. There is a door to the south.',
-    connection: ['bathroom'],
+    description:
+      'when you are done in the kitchen, head to your bedroom to now enjoy your snack and {relax} .',
+    connection: ['kitchen', 'bathroom'],
     inventory: ['chair', 'bed', 'desk', 'lamp'],
   },
 };
 
-console.log(rooms);
+// this is the playerCommands function
+async function playerCommands(commands) {
+  switch (commands) {
+    case 'north':
+      console.log('head north to the garage');
+    case 'lift':
+      console.log(
+        'You have to lift the garage for it to open to get int the basement',
+      );
+      break;
+    case 'open':
+      console.log(
+        'open the basement door it is unlocked and head to the bathroom',
+      );
+      break;
+    case 'wash up':
+      console.log(
+        'go to the bathroom to wash up, and go to the kitchen to make a snack.',
+      );
+      break;
+    case 'snack':
+      console.log('make a sandwich in the kitchen then head to you bedroom.');
+      break;
+    case 'relax':
+      console.log('go to the bedroom to relax');
+      break;
+    default:
+      console.log('I do not understand that command');
+  }
+}
 
 // this is the rooms state object
 roomState = {
@@ -83,7 +112,6 @@ roomState = {
   kitchen: ['bathroom', 'bedroom'],
   bedroom: [],
 };
-console.log(roomState);
 
 // rooms machines of the game room state
 function moveRoom(direction) {
